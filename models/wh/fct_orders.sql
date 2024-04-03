a63ab2afcc089c447dc2c63be149149fefb5cd5d
rajatgupta59@gmail.com <rajatgupta59@gmail.com>
2024-04-03 16:21:13 +0000

    ❇️ New     - Source        - SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000
    🔁 Modified
        - Source
           - tpch
        - Model
           - fct_orders

--- models/wh/fct_orders.sql
+++ models/wh/fct_orders.sql
@@ -1,57 +1,66 @@
 {{
-    config(
-        materialized = 'table'
-    )
+  config({    
+    "materialized": "table"
+  })
 }}
-with orders as (
-    
-    select * from {{ ref('orders') }}
+
+WITH orders_items AS (
+
+  SELECT * 
+  
+  FROM {{ ref('orders_items')}}
 
 ),
-orders_items as (
-    
-    select * from {{ ref('orders_items') }}
+
+order_item_summary AS (
+
+  SELECT 
+    o.order_key,
+    sum(o.gross_item_sales_amount) AS gross_item_sales_amount,
+    sum(o.item_discount_amount) AS item_discount_amount,
+    sum(o.item_tax_amount) AS item_tax_amount,
+    sum(o.net_item_sales_amount) AS net_item_sales_amount
+  
+  FROM orders_items AS o
+  
+  GROUP BY 1
 
 ),
-order_item_summary as (
 
-    select 
-        o.order_key,
-        sum(o.gross_item_sales_amount) as gross_item_sales_amount,
-        sum(o.item_discount_amount) as item_discount_amount,
-        sum(o.item_tax_amount) as item_tax_amount,
-        sum(o.net_item_sales_amount) as net_item_sales_amount
-    from orders_items o
-    group by
-        1
+orders AS (
+
+  SELECT * 
+  
+  FROM {{ ref('orders')}}
+
 ),
-final as (
 
-    select 
+final AS (
 
-        o.order_key, 
-        o.order_date,
-        o.customer_key,
-        o.order_status_code,
-        o.order_priority_code,
-        o.order_clerk_name,
-        o.shipping_priority,
-                
-        1 as order_count,                
-        s.gross_item_sales_amount,
-        s.item_discount_amount,
-        s.item_tax_amount,
-        s.net_item_sales_amount
-    from
-        orders o
-        join
-        order_item_summary s
-            on o.order_key = s.order_key
+  SELECT 
+    o.order_key,
+    o.order_date,
+    o.customer_key,
+    o.order_status_code,
+    o.order_priority_code,
+    o.order_clerk_name,
+    o.shipping_priority,
+    1 AS order_count,
+    s.gross_item_sales_amount,
+    s.item_discount_amount,
+    s.item_tax_amount,
+    s.net_item_sales_amount
+  
+  FROM orders AS o
+  JOIN order_item_summary AS s
+     ON o.order_key = s.order_key
+
 )
-select 
-    f.*,
-    {{ dbt_housekeeping() }}
-from
-    final f
-order by
-    f.order_date
+
+SELECT 
+  f.*,
+  {{ dbt_housekeeping() }}
+
+FROM final AS f
+
+ORDER BY f.order_date
